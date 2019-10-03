eea1dc4d3a3cba0125ee386b04216487dcf6f15c
Keith Kraus <kkraus@nvidia.com>
2019-10-03 11:33:29 -0700

    trying to handle nvcc linking during activate/deactivate

--- recipe/install_nvcc.sh
+++ recipe/install_nvcc.sh
@@ -2,61 +2,66 @@
 
 set -xeuo pipefail
 
-# Default to using `nvcc` to specify `CUDA_HOME`.
-if [ -z ${CUDA_HOME+x} ]
-then
-    CUDA_HOME="$(dirname $(dirname $(which nvcc)))"
-fi
-
 # Set `CUDA_HOME` in an activation script.
 mkdir -p "${PREFIX}/etc/conda/activate.d"
-cat > "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh" <<EOF
+cat >"${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh" <<EOF
 #!/bin/bash
 
-if [[ ! -d "${CUDA_HOME}" ]]
+# Default to using \$(cuda-gdb) to specify \$(CUDA_HOME).
+if [ -z \${CUDA_HOME+x} ]
 then
-    echo "Cannot find: ${CUDA_HOME}"
+    CUDA_HOME="\$(dirname \$(dirname \$(which cuda-gdb)))"
+fi
+
+if [[ ! -d "\${CUDA_HOME}" ]]
+then
+    echo "Cannot find: \${CUDA_HOME}"
     exit 1
 fi
-if [[ ! -f "${CUDA_HOME}/lib64/stubs/libcuda.so" ]]
+if [[ ! -f "\${CUDA_HOME}/lib64/stubs/libcuda.so" ]]
 then
-    echo "Cannot find: ${CUDA_HOME}/lib64/stubs/libcuda.so"
+    echo "Cannot find: \${CUDA_HOME}/lib64/stubs/libcuda.so"
     exit 1
 fi
-grep "CUDA Version ${PKG_VERSION}" ${CUDA_HOME}/version.txt &>/dev/null
-if [[ $? -ne 0 ]]
+grep "CUDA Version \${PKG_VERSION}" \${CUDA_HOME}/version.txt &>/dev/null
+if [[ \$? -ne 0 ]]
 then
     echo "Version of installed CUDA didn't match package"
     exit 1
 fi
 
-export CUDA_HOME="${CUDA_HOME}"
+export CUDA_HOME="\${CUDA_HOME}"
 export CFLAGS="\${CFLAGS} -I\${CUDA_HOME}/include"
 export CPPFLAGS="\${CPPFLAGS} -I\${CUDA_HOME}/include"
 export CXXFLAGS="\${CXXFLAGS} -I\${CUDA_HOME}/include"
+
+# Add \$(libcuda.so) shared object stub to the compiler sysroot.
+# Needed for things that want to link to $(libcuda.so).
+# Stub is used to avoid getting driver code linked into binaries.
+
+CONDA_ENV_SYSROOT="\$(\${CC} --print-sysroot)"
+mkdir -p "\${CONDA_ENV_SYSROOT}/lib"
+ln -s "\${CUDA_HOME}/lib64/stubs/libcuda.so" "\${CONDA_ENV_SYSROOT}/lib/libcuda.so"
 EOF
 
 # Unset `CUDA_HOME` in a deactivation script.
 mkdir -p "${PREFIX}/etc/conda/deactivate.d"
-cat > "${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh" <<EOF
+cat >"${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh" <<EOF
 #!/bin/bash
 unset CUDA_HOME
 unset CFLAGS
 unset CPPFLAGS
 unset CXXFLAGS
+
+# Remove \$(libcuda.so) shared object stub from the compiler sysroot.
+CONDA_ENV_SYSROOT="\$(\${CC} --print-sysroot)"
+rm -f "\${CONDA_ENV_SYSROOT}/lib/libcuda.so"
 EOF
 
 # Create `nvcc` script in `bin` so it can be easily run.
 mkdir -p "${PREFIX}/bin"
-cat > "${PREFIX}/bin/nvcc" <<'EOF'
+cat >"${PREFIX}/bin/nvcc" <<'EOF'
 #!/bin/bash
-"${CUDA_HOME}/bin/nvcc" -ccbin "${CXX}" $@
+"\${CUDA_HOME}/bin/nvcc" -ccbin "\${CXX}" \$@
 EOF
 chmod +x "${PREFIX}/bin/nvcc"
-
-# Add `libcuda.so` shared object stub to the compiler sysroot.
-# Needed for things that want to link to `libcuda.so`.
-# Stub is used to avoid getting driver code linked into binaries.
-CONDA_BUILD_SYSROOT="$(${CC} --print-sysroot)"
-mkdir -p "${CONDA_BUILD_SYSROOT}/lib"
-ln -s "${CUDA_HOME}/lib64/stubs/libcuda.so" "${CONDA_BUILD_SYSROOT}/lib/libcuda.so"
