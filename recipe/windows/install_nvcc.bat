5ef93f270d4f010ddd2fedac0cf649f762d02c15
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-10-31 13:48:39 +0100

    Allow errors in directory creation

--- recipe/windows/install_nvcc.bat
+++ recipe/windows/install_nvcc.bat
@@ -1,15 +1,15 @@
 setlocal enableextensions enabledelayedexpansion || goto :error
 
 :: Activation script
-mkdir %PREFIX%\etc\conda\activate.d || goto :error
+mkdir %PREFIX%\etc\conda\activate.d
 copy %RECIPE_DIR%\windows\activate.bat %PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat || goto :error
 
 :: Deactivation script
-mkdir %PREFIX%\etc\conda\deactivate.d || goto :error
+mkdir %PREFIX%\etc\conda\deactivate.d
 copy %RECIPE_DIR%\windows\deactivate.bat %PREFIX%\etc\conda\deactivate.d\%PKG_NAME%_deactivate.bat || goto :error
 
 :: nvcc executable wrapper
-mkdir %LIBRARY_PREFIX%\bin || goto :error
+mkdir %LIBRARY_PREFIX%\bin
 copy %RECIPE_DIR%\windows\nvcc_windows.bat %LIBRARY_PREFIX%\bin\nvcc.bat || goto :error
 
 :error
