2156b3142688d74fd7db9888da4ffbf2048881cd
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-11-01 15:21:25 +0100

    Retry env vars setting

--- recipe/windows/install_cuda.bat
+++ recipe/windows/install_cuda.bat
@@ -118,11 +118,13 @@ if not "%CUDA_PATCH_URL%"=="" (
 :: Add to PATH
 set "CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v%CUDA_VERSION%"
 
+@echo off
 if "%CI%" == "azure" (
     echo ##vso[task.prependpath]%CUDA_PATH%\bin
-    echo ##vso[task.setvariable variable=CUDA_PATH]%CUDA_PATH%
-    echo ##vso[task.setvariable variable=CUDA_HOME]%CUDA_PATH%
+    echo ##vso[task.setvariable variable=CUDA_PATH;]%CUDA_PATH%
+    echo ##vso[task.setvariable variable=CUDA_HOME;]%CUDA_PATH%
 )
+@echo on
 
 :after_cuda
 echo Continuing with rest of the script...
