05b5ea1e6be925eff18da5400182a8aa55dd758e
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-01-06 11:41:47 +0100

    more cmake_args

--- recipe/install_nvcc.sh
+++ recipe/install_nvcc.sh
@@ -84,7 +84,8 @@ CMAKE_ARGS="\${CMAKE_ARGS:-} -DCUDAToolkit_ROOT=\${CUDA_HOME}"
 ## See https://github.com/conda-forge/nvcc-feedstock/pull/58#issuecomment-752179349
 CMAKE_ARGS+=" -DCUDA_TOOLKIT_ROOT_DIR=\${CUDA_HOME}"
 ## Avoid https://github.com/conda-forge/openmm-feedstock/pull/44#issuecomment-753560234
-CMAKE_ARGS+=" -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=BOTH"
+## We need CUDA_HOME in _front_ of CMAKE_FIND_ROOT_PATH
+CMAKE_ARGS="$(echo \${CMAKE_ARGS} | sed -E -e "s|(-DCMAKE_FIND_ROOT_PATH=)(\S+)|\1\$CUDA_HOME;\2|")"
 export CMAKE_ARGS="\${CMAKE_ARGS}"
 
 ### /CMake configurations
