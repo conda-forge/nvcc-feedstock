9db4cc6605b3075f17d47428cddc753dc4f37051
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-01-10 20:14:18 +0100

    Don't link to .cu on findcudatoolkit without enable_language

--- recipe/cmake-tests/src/gpu.cu
+++ recipe/cmake-tests/src/gpu.cu
@@ -1,11 +1,8 @@
 #include <iostream>
-#include <cuda_runtime.h>
 #include "gpu.hpp"
 
 void printCudaVersion()
 {
-    std::cout << "CUDA Compiled version: " << __CUDACC_VER_MAJOR__ << "." << __CUDACC_VER_MINOR__ << std::endl;
-
     int runtime_ver;
     cudaRuntimeGetVersion(&runtime_ver);
     std::cout << "CUDA Runtime version: " << runtime_ver << std::endl;
--- recipe/cmake-tests/src/main.cc
+++ recipe/cmake-tests/src/main.cc
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <cuda.h>
+
+#ifdef WITHCUFILES
 #include "gpu.hpp"
+#endif
 
 int main()
 {
-    std::cout << "Hello, CUDA!" << std::endl;
+    std::cout << "Hello, CUDA v" << CUDA_VERSION/1000 << "." << CUDA_VERSION/10%100 << std::endl;
+    #ifdef WITHCUFILES
     printCudaVersion();
+    #else
+    std::cout << "Support for driver and runtime versions not available in this test." << std::endl;
+    #endif
 
     return 0;
 }
