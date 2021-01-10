8c7a9fc4d0ef06b3e2b7c11f35dd3fe98771c039
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-01-10 22:09:12 +0100

    oops uncomment that block

--- recipe/windows/test_nvcc.bat
+++ recipe/windows/test_nvcc.bat
@@ -1,76 +1,76 @@
-@REM @echo on
+@echo on
 
-@REM :: Verify the cuda stub library exists.
-@REM if not exist "%LIBRARY_LIB%\cuda.lib" (
-@REM     echo "%LIBRARY_LIB%\cuda.lib is not a file"
-@REM     exit 1
-@REM )
+:: Verify the cuda stub library exists.
+if not exist "%LIBRARY_LIB%\cuda.lib" (
+    echo "%LIBRARY_LIB%\cuda.lib is not a file"
+    exit 1
+)
 
-@REM if not exist "%PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat" (
-@REM     echo "%PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat is not a file"
-@REM     exit 1
-@REM )
+if not exist "%PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat" (
+    echo "%PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat is not a file"
+    exit 1
+)
 
-@REM if not exist "%PREFIX%\etc\conda\deactivate.d\%PKG_NAME%_deactivate.bat" (
-@REM     echo "%PREFIX%\etc\conda\deactivate.d\%PKG_NAME%_deactivate.bat is not a file"
-@REM     exit 1
-@REM )
+if not exist "%PREFIX%\etc\conda\deactivate.d\%PKG_NAME%_deactivate.bat" (
+    echo "%PREFIX%\etc\conda\deactivate.d\%PKG_NAME%_deactivate.bat is not a file"
+    exit 1
+)
 
-@REM :: Try using the activation scripts.
-@REM if defined CUDA_HOME (
-@REM     echo "CUDA_HOME is set to '%CUDA_HOME%'"
-@REM ) else (
-@REM     echo "CUDA_HOME is unset after activation"
-@REM     exit 1
-@REM )
-@REM set "TEST_CUDA_HOME_INITIAL=%CUDA_HOME_CONDA_NVCC_BACKUP%"
+:: Try using the activation scripts.
+if defined CUDA_HOME (
+    echo "CUDA_HOME is set to '%CUDA_HOME%'"
+) else (
+    echo "CUDA_HOME is unset after activation"
+    exit 1
+)
+set "TEST_CUDA_HOME_INITIAL=%CUDA_HOME_CONDA_NVCC_BACKUP%"
 
-@REM call %PREFIX%\etc\conda\deactivate.d\%PKG_NAME%_deactivate.bat
-@REM if errorlevel 1 exit 1
+call %PREFIX%\etc\conda\deactivate.d\%PKG_NAME%_deactivate.bat
+if errorlevel 1 exit 1
 
-@REM if "%CUDA_HOME%"=="" (
-@REM     if "%TEST_CUDA_HOME_INITIAL%"=="" (
-@REM         echo "CUDA_HOME correctly unset after deactivation"
-@REM     )
-@REM ) else (
-@REM     if not "%TEST_CUDA_HOME_INITIAL%"=="" (
-@REM         echo "CUDA_HOME correctly maintained as '%CUDA_HOME%' after deactivation"
-@REM     ) else (
-@REM         echo "CUDA_HOME is incorrectly set to '%CUDA_HOME%' after deactivation"
-@REM         exit 1
-@REM     )
+if "%CUDA_HOME%"=="" (
+    if "%TEST_CUDA_HOME_INITIAL%"=="" (
+        echo "CUDA_HOME correctly unset after deactivation"
+    )
+) else (
+    if not "%TEST_CUDA_HOME_INITIAL%"=="" (
+        echo "CUDA_HOME correctly maintained as '%CUDA_HOME%' after deactivation"
+    ) else (
+        echo "CUDA_HOME is incorrectly set to '%CUDA_HOME%' after deactivation"
+        exit 1
+    )
 
-@REM )
+)
 
 
-@REM :: Manually trigger the activation script
-@REM call %PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat
-@REM if errorlevel 1 exit 1
+:: Manually trigger the activation script
+call %PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat
+if errorlevel 1 exit 1
 
-@REM :: Check activation worked as expected, then deactivate
-@REM if "%CUDA_HOME%"=="" (
-@REM     echo "CUDA_HOME is unset after activation"
-@REM     exit 1
-@REM ) else (
-@REM     echo "CUDA_HOME is set to '%CUDA_HOME%'"
-@REM )
-@REM call %PREFIX%\etc\conda\deactivate.d\%PKG_NAME%_deactivate.bat
-@REM if errorlevel 1 exit 1
+:: Check activation worked as expected, then deactivate
+if "%CUDA_HOME%"=="" (
+    echo "CUDA_HOME is unset after activation"
+    exit 1
+) else (
+    echo "CUDA_HOME is set to '%CUDA_HOME%'"
+)
+call %PREFIX%\etc\conda\deactivate.d\%PKG_NAME%_deactivate.bat
+if errorlevel 1 exit 1
 
-@REM :: If no previous cuda.lib was present, there shouldn't be any!
-@REM if "%CUDALIB_CONDA_NVCC_BACKUP%" == "" (
-@REM     if exist "%LIBRARY_LIB%\cuda.lib" (
-@REM         echo "%LIBRARY_LIB%\cuda.lib" should not exist!
-@REM         exit 1
-@REM     )
-@REM )
+:: If no previous cuda.lib was present, there shouldn't be any!
+if "%CUDALIB_CONDA_NVCC_BACKUP%" == "" (
+    if exist "%LIBRARY_LIB%\cuda.lib" (
+        echo "%LIBRARY_LIB%\cuda.lib" should not exist!
+        exit 1
+    )
+)
 
-@REM :: Reactivate
-@REM call %PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat
-@REM if errorlevel 1 exit 1
+:: Reactivate
+call %PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat
+if errorlevel 1 exit 1
 
-@REM :: Try building something
-@REM call nvcc test.cu
+:: Try building something
+call nvcc test.cu
 
 :: Try different CMake setups
 cd cmake-tests/
