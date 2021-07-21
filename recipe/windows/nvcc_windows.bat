225536ce0d4d5478b2f5f103bdf5e6516390f2be
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-07-21 17:53:04 +0200

    fix windows checks

--- recipe/windows/nvcc_windows.bat
+++ recipe/windows/nvcc_windows.bat
@@ -1,14 +1,12 @@
 @ECHO OFF
-setlocal enabledelayedexpansion
 
 :: check if ccbin is already in the argv
 :: hint: CMD 'string contains' checks are weird; see
 :: https://stackoverflow.com/a/7006016 and
 :: https://stackoverflow.com/a/6310580 for more info
 FOR %%A IN (%*) DO (
-    SET arg=%%A
-    IF NOT x!arg:-ccbin=!==x!arg!             GOTO noccbin
-    IF NOT x!arg:--compiler-bindir=!==x!arg!  GOTO noccbin
+    ECHO %%A | FINDSTR /C:"-ccbin"            >nul && ( GOTO noccbin )
+    ECHO %%A | FINDSTR /C:"--compiler-bindir" >nul && ( GOTO noccbin )
 )
 :: ccbin was not passed already; we add it ourselves
 GOTO ccbin
