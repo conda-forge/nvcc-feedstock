d2877b3493b4bfb8465f0206927fd5959dd67137
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-07-21 16:06:03 +0200

    skip ccbin if passed on windows too

--- recipe/windows/nvcc_windows.bat
+++ recipe/windows/nvcc_windows.bat
@@ -1,1 +1,14 @@
+@ECHO OFF
+:: check if ccbin is already in the argv
+FOR %%A IN (%*) DO (
+    IF %%A=="-ccbin" GOTO noccbin
+)
+:: ccbin was not passed already; we add it ourselves
+GOTO ccbin
+
+:noccbin
+"%CUDA_HOME%\bin\nvcc.exe" %*
+GOTO :EOF
+
+:ccbin
 "%CUDA_HOME%\bin\nvcc.exe" -ccbin "%CXX%" %*
