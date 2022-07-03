e8a9c6bdf4ab39bcee3ae007e045c15a7884b723
Leo Fang <leo80042@gmail.com>
2022-07-02 23:06:37 -0400

    Try escape

--- recipe/windows/nvcc_windows.bat
+++ recipe/windows/nvcc_windows.bat
@@ -1,2 +1,2 @@
-for /f "tokens=* usebackq" %%f in (`where cl.exe`) do (set "CONDA_NVCC_CCBIN=%%~dpf")
+for /f "tokens=* usebackq" %%f in (`where cl.exe`) do (set "CONDA_NVCC_CCBIN=%%~dpf:\=\\%%")
 "%CUDA_HOME%\bin\nvcc.exe" --use-local-env -ccbin "%CONDA_NVCC_CCBIN%" %*
