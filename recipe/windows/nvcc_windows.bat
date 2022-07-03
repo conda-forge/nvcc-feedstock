6b43755ecf70d804674aa72eada4e84ed51bb3bb
Leo Fang <leo80042@gmail.com>
2022-07-02 22:34:56 -0400

    set ccbin to the folder containing cl.exe, not to cl.exe itself

--- recipe/windows/nvcc_windows.bat
+++ recipe/windows/nvcc_windows.bat
@@ -1,1 +1,2 @@
-"%CUDA_HOME%\bin\nvcc.exe" --use-local-env -ccbin "%CXX%" %*
+for /f "tokens=* usebackq" %%f in (`where cl.exe`) do (set "CONDA_NVCC_CCBIN=%%~dpf")
+"%CUDA_HOME%\bin\nvcc.exe" --use-local-env -ccbin "%CONDA_NVCC_CCBIN%" %*
