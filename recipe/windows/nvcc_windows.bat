2f97d3b749f4516c078562f1a82360346aa1fb5d
Leo Fang <leo80042@gmail.com>
2022-07-26 11:33:09 -0400

    try again, full path to cl.exe without escape

--- recipe/windows/nvcc_windows.bat
+++ recipe/windows/nvcc_windows.bat
@@ -1,2 +1,3 @@
-for /f "tokens=* usebackq" %%f in (`where cl.exe`) do (set "dummy=%%f" && call set "CONDA_NVCC_CCBIN=%%dummy:\=\\%%")
+echo %PATH%
+for /f "tokens=* usebackq" %%f in (`where cl.exe`) do set "CONDA_NVCC_CCBIN=%%f"
 "%CUDA_HOME%\bin\nvcc.exe" --use-local-env -ccbin "%CONDA_NVCC_CCBIN%" %*
