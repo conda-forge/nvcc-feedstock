dfa1726319df4826066c1eb64704ff82ce7c82d5
Leo Fang <leo80042@gmail.com>
2022-07-27 20:10:54 -0400

    Revert to follow POSIX convention
    Co-authored-by: jakirkham <jakirkham@gmail.com>

--- recipe/linux/activate.sh
+++ recipe/linux/activate.sh
@@ -60,7 +60,7 @@ fi
 
 if [[ -z "$(${CUDA_HOME}/bin/nvcc --version | grep "Cuda compilation tools, release __PKG_VERSION__")" ]]
 then
-  if [[ "${CONDA_BUILD}" == "1" ]]
+  if [ "${CONDA_BUILD}" = "1" ]
   then
     echo "Error: Version of installed CUDA didn't match package"
     return 1
